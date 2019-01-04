710cff67f04b4703267c5a990cac305a11c45783
Mikhail Gekhman <GehmanmM@yandex.ru>
2019-01-04 22:29:05 +0500

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+import utils from '../utils';
+
+const isPrime = (num) => {
+  if (num === 1) return false;
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const description = 'Enter "yes" if the number is prime and "no" otherwise';
+const playRound = () => {
+  const number = utils.getRandomNum(1, 25);
+  const userAnswer = readlineSync.question(`Is ${number} is prime?  `);
+  const rightAnswer = isPrime(number) ? 'yes' : 'no';
+  return utils.checkAnswer(userAnswer, rightAnswer);
+};
+
+const primeGame = () => {
+  const rounds = 3;
+  let scoreCounter = 0;
+  console.log(description);
+  for (let i = 0; i < rounds; i += 1) {
+    if (playRound()) scoreCounter += 1;
+  }
+  utils.printResults(scoreCounter, rounds);
+};
+
+export default primeGame;
