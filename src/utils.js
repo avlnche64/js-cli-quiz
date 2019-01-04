76e1a1cdc58a3846fe7f7daa0e9ef4704ec96590
Mikhail Gekhman <GehmanmM@yandex.ru>
2019-01-04 22:26:24 +0500

    add  utils

---
+++ src/utils.js
@@ -0,0 +1,39 @@
+const utils = {};
+
+utils.getRandomNum = (min, max) => {
+  const result = min + Math.floor(Math.random() * (max + 1 - min));
+  return result;
+};
+
+utils.checkAnswer = (userAnswer, rightAnswer) => {
+  if (userAnswer === rightAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log('It is wrong answer :(');
+  return false;
+};
+
+utils.printResults = (scores, rounds) => {
+  const ratio = scores / rounds;
+  switch (true) {
+    case (ratio === 1):
+      console.log(`Congradulations! You gave ${scores} correct answers.`);
+      break;
+    case (ratio >= 0.6):
+      console.log(`Not bad! You gave ${scores} correct answers.`);
+      break;
+    case (ratio >= 0.3):
+      console.log(`You gave ${scores} correct answers. You should make more efforts next time`);
+      break;
+    case (ratio >= 0):
+      console.log(`Sorry :( You gave ${scores} correct answers. Please try again`);
+      break;
+    default:
+      // do nothing
+  }
+};
+// utils.checkAnswer = (answer, rightAnswer) => {
+//   if (answer === rightAnswer) return
+// }
+export default utils;
