fdedbd4825a0473b4c8831fd3d0e186e828969eb
Mikhail Gekhman <GehmanmM@yandex.ru>
2019-01-05 20:41:37 +0500

    add calc game

--- src/games.js
+++ src/games.js
@@ -1,7 +1,9 @@
 import prime from './games/prime';
+import calc from './games/calc';
 
 const games = {
   prime,
+  calc,
 };
 
 export default games;
---
+++ src/games/calc.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+import utils from '../utils';
+
+const getExpression = (numberOfSummands) => {
+  const summands = [];
+  for (let i = 0; i < numberOfSummands; i += 1) {
+    summands.push(utils.getRandomNum(0, 99));
+  }
+  return summands.join(' + ');
+};
+
+const description = 'What is the result of expression?';
+const playRound = () => {
+  const expression = getExpression(3);
+  const userAnswer = +readlineSync.question(`Expression: ${expression} = `);
+  const rightAnswer = utils.sumArrElements(expression.split(' + '));
+  return utils.checkAnswer(userAnswer, rightAnswer);
+};
+
+const calc = () => {
+  const rounds = 3;
+  let scoreCounter = 0;
+  console.log(description);
+  for (let i = 0; i < rounds; i += 1) {
+    if (playRound()) scoreCounter += 1;
+  }
+  utils.printResults(scoreCounter, rounds);
+};
+
+export default calc;
