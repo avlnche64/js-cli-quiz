830982d01d00234e3623e1cfdb2fda3d2f89f996
Mikhail Gekhman <GehmanM@yandex.ru>
2019-01-07 14:53:04 +0500

    add even game

--- src/games.js
+++ src/games.js
@@ -1,9 +1,11 @@
 import prime from './games/prime';
 import calc from './games/calc';
+import even from './games/even';
 
 const games = {
   prime,
   calc,
+  even,
 };
 
 export default games;
---
+++ src/games/even.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+import utils from '../utils';
+
+const isEven = num => num % 2 === 0;
+
+const description = 'Enter "yes" if the number is even and "no" otherwise';
+const playRound = () => {
+  const number = utils.getRandomNum(1, 200);
+  const userAnswer = readlineSync.question(`Is ${number} an even number? `);
+  const rightAnswer = isEven(number) ? 'yes' : 'no';
+  return utils.checkAnswer(userAnswer, rightAnswer);
+};
+
+const even = () => {
+  const rounds = 3;
+  let scoreCounter = 0;
+  console.log(description);
+  for (let i = 0; i < rounds; i += 1) {
+    if (playRound()) scoreCounter += 1;
+  }
+  utils.printResults(scoreCounter, rounds);
+};
+
+export default even;
