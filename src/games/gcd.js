281b462c0c6ff72d43e316fab7bd004f04955654
Mikhail Gekhman <GehmanmM@yandex.ru>
2019-01-12 16:23:30 +0500

    modify gcd-game with using game-engine

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,23 +1,13 @@
-import readlineSync from 'readline-sync';
 import * as utils from '../utils';
+import { createGameLauncher, createGameRound } from '../game-engine';
+
+const getTwoRandomNums = () => [utils.getRandomNum(1, 60), utils.getRandomNum(1, 60)];
 
 const description = '\nFind the greatest common divisor (GCD) of two numbers';
-const playRound = () => {
-  const firstNumber = utils.getRandomNum(1, 60);
-  const secondNumber = utils.getRandomNum(1, 60);
-  const userAnswer = +readlineSync.question(`Numbers: ${firstNumber} ${secondNumber}\nGCD: `);
-  const rightAnswer = utils.getGreatestCommonDivisor(firstNumber, secondNumber);
-  return utils.checkAnswer(userAnswer, rightAnswer);
-};
+const getQuestion = () => getTwoRandomNums().join(' ');
+const getRightAnswer = question => `${utils.getGreatestCommonDivisor(...question.split(' '))}`;
 
-const gcd = () => {
-  const rounds = 3;
-  let scores = 0;
-  console.log(description);
-  for (let i = 0; i < rounds; i += 1) {
-    if (playRound()) scores += 1;
-  }
-  utils.printResults(scores, rounds);
-};
+const playRound = createGameRound(getQuestion, getRightAnswer);
+const startGame = createGameLauncher(3, description, playRound);
 
-export default gcd;
+export default startGame;
