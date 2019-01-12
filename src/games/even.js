c577d451094358f64fac7c3ae18c1b2a8eac5db2
Mikhail Gekhman <GehmanmM@yandex.ru>
2019-01-12 15:57:25 +0500

    modify even-game with using game-engine

--- src/games/even.js
+++ src/games/even.js
@@ -1,24 +1,12 @@
-import readlineSync from 'readline-sync';
 import * as utils from '../utils';
+import { createGameLauncher, createGameRound } from '../game-engine';
 
 const isEven = num => num % 2 === 0;
 
 const description = 'Enter "yes" if the number is even and "no" otherwise';
-const playRound = () => {
-  const number = utils.getRandomNum(1, 200);
-  const userAnswer = readlineSync.question(`Is ${number} an even number? `);
-  const rightAnswer = isEven(number) ? 'yes' : 'no';
-  return utils.checkAnswer(userAnswer, rightAnswer);
-};
+const getQuestion = () => utils.getRandomNum(1, 200);
+const getRightAnswer = question => utils.boolToYesNo(isEven(question));
 
-const even = () => {
-  const rounds = 3;
-  let scoreCounter = 0;
-  console.log(description);
-  for (let i = 0; i < rounds; i += 1) {
-    if (playRound()) scoreCounter += 1;
-  }
-  utils.printResults(scoreCounter, rounds);
-};
-
-export default even;
+const playRound = createGameRound(getQuestion, getRightAnswer);
+const startGame = createGameLauncher(3, description, playRound);
+export default startGame;
