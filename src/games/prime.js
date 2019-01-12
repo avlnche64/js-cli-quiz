809c6ae215bff3e36ee99c1f7fea2e969d91ee84
Mikhail Gekhman <GehmanmM@yandex.ru>
2019-01-12 14:55:45 +0500

    modify prime-game with using game-engine

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,5 +1,5 @@
-import readlineSync from 'readline-sync';
 import * as utils from '../utils';
+import { createGameLauncher, createGameRound } from '../game-engine';
 
 const isPrime = (num) => {
   if (num === 1) return false;
@@ -10,21 +10,9 @@ const isPrime = (num) => {
 };
 
 const description = 'Enter "yes" if the number is prime and "no" otherwise';
-const playRound = () => {
-  const number = utils.getRandomNum(1, 25);
-  const userAnswer = readlineSync.question(`Is ${number} a prime number? `);
-  const rightAnswer = isPrime(number) ? 'yes' : 'no';
-  return utils.checkAnswer(userAnswer, rightAnswer);
-};
+const getQuestion = () => utils.getRandomNum(1, 25);
+const getRightAnswer = question => utils.boolToYesNo(isPrime(question));
 
-const primeGame = () => {
-  const rounds = 3;
-  let scoreCounter = 0;
-  console.log(description);
-  for (let i = 0; i < rounds; i += 1) {
-    if (playRound()) scoreCounter += 1;
-  }
-  utils.printResults(scoreCounter, rounds);
-};
-
-export default primeGame;
+const playRound = createGameRound(getQuestion, getRightAnswer);
+const startGame = createGameLauncher(3, description, playRound);
+export default startGame;
